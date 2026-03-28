68fce0f36418922aa4b8286044e5804be3bded7c
Amir Rahi <amirrahi29@gmail.com>
2026-03-28 21:16:58 +0530

    first commit

---
+++ src/main/java/com/interview/app/MyDemoController.java
@@ -0,0 +1,15 @@
+package com.interview.app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class MyDemoController {
+
+    @GetMapping("/test")
+    public String data(){
+        return "my test data controller (amir rahi)";
+    }
+}
